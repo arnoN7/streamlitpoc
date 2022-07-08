782c3f08a65d39fb4e2f5def89ba81222ba11c21
arnoN7 <arnaud.rover@gmail.com>
2022-07-08 14:35:06 +0200

    Try simple title page

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ SIMULATION_TABLE = 'STG_SIMULATION'
 ## Initialize connection.
 # Uses st.experimental_singleton to only run once.
 
-session = Session.builder.configs(st.secrets["snowflake"]).create()
-df_simul_pd = session.table('STG_SIMULATION').to_pandas()
+#session = Session.builder.configs(st.secrets["snowflake"]).create()
+#df_simul_pd = session.table('STG_SIMULATION').to_pandas()
 
 '''def update_table(df, table_name):
     new_simul = session.create_dataframe(df)
