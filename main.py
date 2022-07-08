7d2e13bcad8872bcad695cfec42a8b2f67615aad
arnoN7 <arnaud.rover@gmail.com>
2022-07-08 17:00:32 +0200

    Try simple title page

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ SIMULATION_TABLE = 'STG_SIMULATION'
 st.title("tesddt")
 session = Session.builder.configs(st.secrets["snowflake"]).create()
 df_simul = session.table('STG_SIMULATION')
-#df_simul_pd = df_simul.to_pandas()
+df_simul_pd = df_simul.to_pandas()
 
 '''def update_table(df, table_name):
     new_simul = session.create_dataframe(df)
